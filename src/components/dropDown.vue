0f33521e6ce0333f0c724381578657f2c33e7408
stephanielamm <slamm5@live.unc.edu>
2017-02-14 23:32:18 -0500

    Added select form but not yet bound.
    Still need to bind data using dropDown.vue.

---
+++ src/components/dropDown.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+  {{ message }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  props: [
+    'tweetData'
+  ],
+  data () {
+    return {
+      message: 'tweetData message test'
+    }
+  },
+  mounted () {
+    console.log(this.tweets)
+  }
+}
+</script>
+
+<style>
+</style>
