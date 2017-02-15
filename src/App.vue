9c2e16bb316fd0578f78f8f513c78859c845c089
stephanielamm <slamm5@live.unc.edu>
2017-02-15 02:27:26 -0500

    Preparing inputs for binding.
    I had to fix my hyphenated category in my Json file.

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,76 @@
 <template>
   <div id="app">
     <!-- Dropdown menu to set company -->
-    <p>Company:
+    <p id="drop-down-menu">Company:
     <select v-model="company" class="company">
-      <option value="">Select a Company</option>
-      <option value="Boeing - BA">Boeing</option>
-      <option value="Fiat-Chrysler - FCA">Fiat-Chrysler</option>
-      <option value="Ford - F">Ford</option>
-      <option value="Toyota">Toyota</option>
-      <option value="Nordstrom">Nordstrom</option>
-      <option value="Rexnord">Rexnord</option>
-      <option value="United Technologies">United Technologies</option>
-      <option value="General Motors">General Motors</option>
+      <option value="Select a company to see tweets and stock data">Select a Company</option>
+      <option value="Boeing (BA)">Boeing</option>
+      <option value="Fiat-Chrysler (FCAU)">Fiat-Chrysler</option>
+      <option value="Ford (F)">Ford</option>
+      <option value="Toyota (TM)">Toyota</option>
+      <option value="Nordstrom (JWN)">Nordstrom</option>
+      <option value="Rexnord (RXN)">Rexnord</option>
+      <option value="United Technologies (UTX)">United Technologies</option>
+      <option value="General Motors (GM)">General Motors</option>
     </select>
   </br>
-       <span id="company-name">{{ company }}</span>
+    <div class="container" id="company-container">
+      <div class="row">
+      <div class="col-6" id="company-name">{{ company }}</div>
+      <div class="col-6" id="company-logo">
+        <img src="static/boeing-logo.png" alt="Boeing Logo" style="width:300px;height:100px;">
+        </div>
+    </div>
+    <div class="row" id="company-about">
+      <p> {{ about }}</p>
+      </div>
+    <div class="row">
+    <div class="col-6" id="tweet-box">
+    {{ tweettext }}
+      </div>
+    <div class="col-6" id="stock-chart">
+      This is where the stock chart will go.
+    <!--  <div id="container" style="height: 350px; min-width: 510px"></div>
+      <script>
+      // code from HighStocks; will need to figure out how to get relevant stock data
+      $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+        // Create the chart
+      Highcharts.stockChart('container', {
+        rangeSelector: {
+            selected: 1
+        },
+        title: {
+            text: 'AAPL Stock Price'
+        },
+        series: [{
+            name: 'AAPL',
+            data: data,
+            tooltip: {
+                valueDecimals: 2
+            }
+        }]
+    });
+});
+</script> -->
+      <!-- will have component for stock chart (made with Highstock API) (to right of the page) -->
+      </div>
+        </div>
+      <div class="row" id="explanatory-text">
+      <div class="col-12">
+      <p>{{ analysis }}</p>
+      <!-- will have component for explanatory text (under the visual elements) -->
+    <explanatoryText></explanatoryText>
+    </div>
+  </div>
+  </div>
       </p>
-  <!--  <tweetData :tweetData="tweets">
-  </tweetData> -->
+  <tweetData
+  :company="company"
+  :about="about"
+  :tweet-text="tweettext"
+  :analysis="analysis"
+  >
+  </tweetData>
   </div>
 </template>
 
@@ -30,17 +83,20 @@ export default {
   components: {
     tweetData
   },
+
   data () {
     return {
       message: 'test message',
       tweets: [],
-      company: ''
+      company: '',
+      about: 'Choose a company using the drop down menu and text about the company will appear here.',
+      analysis: 'Choose a company using the drop down menu and text analyzing the stock price will appear here',
+      tweettext: 'Choose a company using the drop down menu and a tweet from @realDonaldTrump mentioning that company will appear here.'
     }
   },
   mounted () {
     axios.get('/static/twitter-data.json')
     .then((response) => {
-      // console.log(response.data)
       this.tweets = response.data
     })
   }
@@ -50,13 +106,20 @@ export default {
 <style>
 #app {
   font-family: 'Raleway', Helvetica, Arial, sans-serif;
-  font-size: 25px;
+  font-size: 18px;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   margin-top: 20px;
   padding:20px;
+  background-color: #F5F8FA;
   /* border: thin grey solid; */
+}
+#drop-down-menu {
+  margin-top: 2em;
+  margin-left: 3.5em;
+  margin-bottom: 2em;
+  text-align: center;
+  font-size: 25px;
 }
 #company-name {
   font-family: 'Mukta Vaani', sans-serif;
@@ -65,5 +128,35 @@ export default {
   float: left;
   margin-left: 2em;
   margin-top: 2em;
+}#company-logo {
+  float: right;
+  margin-top: 1em;
+  margin-right: 1em;
+}
+#company-about {
+  font-family: 'Raleway', Helvetica, Arial, sans-serif;
+  margin-left: 0.5em;
+  margin-right: 0.5em;
+  margin-top: 0.1em;
+}
+/* Twitter Styling */
+#tweet-box {
+  float: left;
+  margin-left: 3.5em;
+  margin-top: 2em;
+  margin-bottom: 2em;
+}
+/* Stock Chart Styling */
+#stock-chart {
+  float: right;
+  margin-right: 2em;
+  margin-top: 2em;
+  margin-bottom: 2em;
+}
+/* Explanatory Text Box Styling */
+#explanatory-text {
+  font-family: 'Raleway', Helvetica, Arial, sans-serif;
+  margin-left: 0.5em;
+  margin-right: 0.5em;
 }
 </style>
